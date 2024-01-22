68498ef5fe497de37628206b1bbb61e88aa3ef3a
Sezgin Kipel <1444sezgin@gmail.com>
2024-01-22 22:38:55 +0300

    Update account-addresses.php

--- views/user/account-addresses.php
+++ views/user/account-addresses.php
@@ -9,12 +9,10 @@ if (!isset($_SESSION['user'])) {
     $user = $_SESSION['user'];
 }
 
-
-$orderQuery = $db->prepare('SELECT * FROM orders WHERE customerID = :customerID');
-$orderQuery->bindParam(':customerID', $user->id);
-$orderQuery->execute();
-
-$orders = $orderQuery->fetchAll(PDO::FETCH_OBJ);
+$getAddresses = $db->prepare('SELECT * FROM addresses WHERE customerId = :customerId');
+$getAddresses->bindParam(':customerId', $user->id);
+$getAddresses->execute();
+$addresses = $getAddresses->fetchAll(PDO::FETCH_OBJ);
 ?>
 
 <section id="user_account">
@@ -33,24 +31,55 @@ $orders = $orderQuery->fetchAll(PDO::FETCH_OBJ);
             <div class="col-12 col-md-3">
                 <ul class="nav nav-pills flex-column">
                     <li class="nav-item">
-                        <a class="nav-link" href="<?=BASE_URL?>hesabim">Siparişlerim</a>
+                        <a class="nav-link" href="<?= BASE_URL ?>hesabim">Siparişlerim</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="<?=BASE_URL?>hesabim/duzenle">Bilgilerimi Güncelle</a>
+                        <a class="nav-link" aria-current="page" href="<?= BASE_URL ?>hesabim/duzenle">Bilgilerimi
+                            Güncelle</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link active" href="<?=BASE_URL?>hesabim/adreslerim">Adreslerim</a>
+                        <a class="nav-link active" href="<?= BASE_URL ?>hesabim/adreslerim">Adreslerim</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?=BASE_URL?>hesabim/iletisim-bilgilerim">İletişim Bilgilerim</a>
+                        <a class="nav-link" href="<?= BASE_URL ?>hesabim/iletisim-bilgilerim">İletişim Bilgilerim</a>
                     </li>
                 </ul>
             </div>
 
             <div class="col-12 col-md-9">
-                <h1>ADRESLERİM!</h1>
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="fs-3">Adreslerim</h1>
+                        <hr>
+                    </div>
+                </div>
+
+                <div class="row row-cols-1 column-gap-3">
+                    <?php foreach ($addresses as $address){ ?>
+                    <div class="col">
+                        <div class="card">
+                            <h5 class="card-header"><?=$address->title?></h5>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <?=$address->customerName?>
+                                </h5>
+                                <p class="card-text"><?=$address->addressDetails?></p>
+                                <small class="card-text"><?=$address->city?></small>
+                                /
+                                <small class="card-text"><?=$address->postCode?></small>
+                            </div>
+
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-warning">Adresi Düzenle</a>
+                                <a href="#" class="btn btn-danger">Adresi Sil</a>
+                            </div>
+                        </div>
+                    </div>
+                    <?php } ?>
+                </div>
+
             </div>
         </div>
 
