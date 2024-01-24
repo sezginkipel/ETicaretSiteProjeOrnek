e35b53cabfd428ef2f7faf4ac3200d9656204547
Sezgin Kipel <1444sezgin@gmail.com>
2024-01-24 05:05:16 +0300

    Update adminController.php

--- controllers/adminController.php
+++ controllers/adminController.php
@@ -1,16 +1,16 @@
 <?php
 $router = new Router();
 
-$router->get('yonetim-paneli', function (){
+$router->get('yonetim-paneli', function () {
     loadView('admin/index/index', 'Yönetim Paneli');
 });
 
 /*| Ürünlerle ilgili yönetimsel işlemler |*/
-$router->get('yonetim-paneli/urunler/ekle', function (){
+$router->get('yonetim-paneli/urunler/ekle', function () {
     loadView('admin/product/add', 'Ürün Ekle');
 });
 
-$router->post('yonetim-paneli/urunler/ekle', function (){
+$router->post('yonetim-paneli/urunler/ekle', function () {
     global $db;
     $reqData = json_decode(file_get_contents('php://input'));
 
@@ -22,18 +22,68 @@ $router->post('yonetim-paneli/urunler/ekle', function (){
 
     $addProduct = $db->prepare('INSERT INTO products SET title = ?, price = ?, imgURL = ?, categoryID = ?, description = ?, slug = ?');
 
-    if($addProduct->execute([$title, $price, $img, $categoryId, $description, createSlug($title)]))
-    {
+    if ($addProduct->execute([$title, $price, $img, $categoryId, $description, createSlug($title)])) {
         header('Content-Type: application/json');
         echo json_encode(['status' => 'success', 'message' => 'Ürün başarıyla eklendi.']);
-    }
-    else
-    {
+    } else {
         header('Content-Type: application/json');
         echo json_encode(['status' => 'error', 'message' => 'Ürün eklenirken bir hata oluştu.']);
     }
+});
 
+$router->post('yonetim-paneli/urunler/gorsel-yukle', function () {
 
+    global $db;
+    $reqData = json_decode(file_get_contents('php://input'));
+
+    $file = $_FILES['product_imgFile'];
+    $fileName = $file['name'];
+    $fileTmpName = $file['tmp_name'];
+    $fileSize = $file['size'];
+    $fileError = $file['error'];
+
+    $fileExt = explode('.', $fileName);
+
+    $fileActualExt = strtolower(end($fileExt));
+    $allowed = ['jpg', 'jpeg', 'png', 'svg', 'webp'];
+
+    if (in_array($fileActualExt, $allowed)) {
+        if ($fileError === 0) {
+            if ($fileSize < 1000000) {
+                $fileNameNew = uniqid('', true) . '.' . $fileActualExt;
+                $fileDestination = BASE_DIRECTORY . 'public/uploads/' . $fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                $response = [
+                    'status' => 'success',
+                    'message' => 'Dosya başarıyla yüklendi.',
+                    'fileUrl' => BASE_URL . 'public/uploads/' . $fileNameNew
+                ];
+                header('Content-Type: application/json');
+                echo json_encode($response);
+            } else {
+                $response = [
+                    'status' => 'error',
+                    'message' => 'Dosya boyutu çok büyük.'
+                ];
+                header('Content-Type: application/json');
+                echo json_encode($response);
+            }
+        } else {
+            $response = [
+                'status' => 'error',
+                'message' => 'Dosya yüklenirken bir hata oluştu.'
+            ];
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }
+    } else {
+        $response = [
+            'status' => 'error',
+            'message' => 'Bu dosya türü desteklenmiyor.'
+        ];
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
 
 
 });
